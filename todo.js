fbd41ef4b262945e9d9834951c17f82d858fda2a
Yakumaa <maharjanshrish8@gmail.com>
2025-01-20 13:54:45 +0545

    Implement basic todo app functionality with AngularJS

--- todo.js
+++ todo.js
@@ -1,3 +1,36 @@
-function TodoCtrl($scope) {
-  $scope.totalTodos = 4;
-}
+angular.module('todoApp', [])
+  .controller('todoCtrl', function($scope) {
+
+    $scope.todos = [
+      {text: "Learn AngularJS", done: false},
+      {text: "Build an app", done: false},
+      {text: "Deploy to cloud", done: false},
+    ];
+
+    $scope.getTotalTodos = function () {
+      return $scope.todos.length;
+    }
+
+    $scope.remaining = function () {
+      var count = 0;
+      angular.forEach($scope.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      })
+      return count;
+    }
+
+    $scope.addTodo = function () {
+      $scope.todos.push({text:$scope.todoText, done:false});
+      $scope.todoText = '';
+      $scope.totalTodos++;
+    }
+
+    $scope.remove = function () {
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) $scope.todos.push(todo);
+      });
+    }
+  });
+
