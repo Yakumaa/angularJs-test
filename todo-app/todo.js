1fadfaa783a46f32f9786305bbc88c5233e6307c
Yakumaa <maharjanshrish8@gmail.com>
2025-01-21 17:44:47 +0545

    Refactor todo app structure and add search functionality

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -1,36 +1,49 @@
-angular.module('todoApp', [])
-  .controller('todoCtrl', function($scope) {
+var todoApp = angular.module('todoApp', [])
 
-    $scope.todos = [
-      {text: "Learn AngularJS", done: false},
-      {text: "Build an app", done: false},
-      {text: "Deploy to cloud", done: false},
-    ];
+// todoApp.config(function ($routeProvider) {
+//   $routeProvider
+//     .when('/', {
+//       controller: 'todoCtrl',
+//       templateUrl: 'index.html'
+//     })
+//     .when('/view2', {
+//       controller: 'todoCtrl',
+//       templateUrl: 'view2.html'
+//     })
+//     .otherwise({redirectTo: '/'});
+// });
 
-    $scope.getTotalTodos = function () {
-      return $scope.todos.length;
-    }
+todoApp.controller('todoCtrl', function($scope) {
+  $scope.todos = [
+    {text: "Learn AngularJS", done: false},
+    {text: "Build an app", done: false},
+    {text: "Deploy to cloud", done: false},
+  ];
 
-    $scope.remaining = function () {
-      var count = 0;
-      angular.forEach($scope.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      })
-      return count;
-    }
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  }
 
-    $scope.addTodo = function () {
-      $scope.todos.push({text:$scope.todoText, done:false});
-      $scope.todoText = '';
-      $scope.totalTodos++;
-    }
+  $scope.remaining = function () {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    })
+    return count;
+  }
 
-    $scope.remove = function () {
-      var oldTodos = $scope.todos;
-      $scope.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
-    }
-  });
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.todoText = '';
+    $scope.totalTodos++;
+  }
+
+  $scope.remove = function () {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  }
+});
 
